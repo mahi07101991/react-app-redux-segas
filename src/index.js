5c89e673a15b6c7da41149b1e6d0a3c410115ccd
Mahesh <mahi07101991@gmail.com>
2023-08-15 05:17:27 +0900

    initial setup installation of routes-dom, react-redux, react-sagas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import ErrorPage from "./error-page";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
 import './styles/main.scss';
 
-ReactDOM.render(<h1>Helloworld React!</h1>, document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
